efacfaaeaf73fec7a88192ad21c682a6c4323c63
kearaharrington <keara.harrington@gmail.com>
2022-07-27 19:52:08 -0700

    wrote function for displaying player stats - still need to fix formatting

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const die = document.getElementById('roll');
 const rollButton = document.getElementById('die-button');
 const rollDisplay = document.getElementById('number');
-const journeyButtons = document.getElementById('game-buttons')
+const journeyButtons = document.getElementById('game-buttons');
+const playerInfo = document.getElementById('player-info');
 let rollResult;
 
 // ====================== PAINT INTIAL SCREEN ======================= //
@@ -11,14 +12,55 @@ let rollResult;
 rollButton.addEventListener("click", rollDie);
 
 // ====================== ENTITIES ======================= //
-let player = {
-    name: '',
-    class: '',
-    maxHP: 100,
-    currentHP: 100,
-    hasEaten: false,
-    alive: true,
-}
+class Player {
+    // constructor(playerName) {
+    constructor() {
+        // this.name = playerName,
+        // this.class = '',
+        this.maxHP = 100,
+        this.currentHP = 100,
+        this.maxStealth = 100,
+        this.stealth = 100,
+        this.hasEaten = false,
+        this.alive = true
+    };
+};
+
+let player1 = new Player();
+
+function displayPlayerInfo() {
+    let life = document.createElement('p');
+    playerInfo.appendChild(life);
+    life.innerText = `Alive: ${player1.alive}`;
+
+    let hp = document.createElement('p');
+    playerInfo.appendChild(hp);
+    // hp.setAttribute('id', 'HP');
+    // let playerHp = document.getElementById('HP');
+    hp.innerText = `HP: ${player1.currentHP}`;
+    
+    let stealth = document.createElement('p');
+    playerInfo.appendChild(stealth);
+    // stealth.setAttribute('id', 'stealth');
+    // let stealthValue = document.getElementById('stealth');
+    stealth.innerText = `Stealth: ${player1.stealth}`;
+
+    let eat = document.createElement('p');
+    playerInfo.appendChild(eat);
+    // eat.setAttribute('id', 'snacked');
+    playerInfo.appendChild(eat);
+
+    function snacked() {
+        if (player1.hasEaten) {
+            eat.innerText = 'Status: Successfully snacked!'
+        } else {
+            eat.innerText = 'Status: Still hungry...'
+        }
+    }
+    snacked();
+};
+
+displayPlayerInfo();
 
 // ====================== GAME PROCESSES ======================= //
 
@@ -28,15 +70,19 @@ function rollDie() {
     rollDisplay.textContent = rollResult;
     // return rollResult;
     console.log(rollResult);
-}
+};
 
 // sneak past kids' rooms
 function sneak() {
     
-}
+};
 
 // go upstairs
+function goUpstairs() {
+
+};
 
 // get snack
+
 
 // reset back to start
